aaaa675aedb73a80a7616bb51fe0602eb5a9d10c
Ben Best <ben@ecoquants.com>
2024-09-06 01:28:36 +0200

    r pkg: -worms, +worrms

--- lib/pre-render.R
+++ lib/pre-render.R
@@ -1,19 +1,19 @@
 # load libraries
 librarian::shelf(
-  glue, here, rsvg, stringr, quiet = T)
+  glue, rsvg, stringr, quiet = T)
 
 # update Github repo contributors image
 img_url <- "https://contrib.rocks/image?repo=ioos/bio_data_guide"
-img_svg <- here("figs/contrib.rocks.svg")
-img_png <- here("figs/contrib.rocks.png")
+img_svg <- "figs/contrib.rocks.svg"
+img_png <- "figs/contrib.rocks.png"
 download.file(img_url, img_svg)
 rsvg_png(img_svg, img_png)
 
 # update dataset-edna/README.md (in git submodule) with downloaded Binder svg
 doc_md  <- "datasets/dataset-edna/README.md"
 img_url <- "https://mybinder.org/badge_logo.svg"
-img_svg <- here("figs/mybinder.org_badge_logo.svg")
-img_png <- here("figs/mybinder.org_badge_logo.png")
+img_svg <- "figs/mybinder.org_badge_logo.svg"
+img_png <- "figs/mybinder.org_badge_logo.png"
 if (!file.exists(img_png)){
   download.file(img_url, img_svg)
   rsvg_png(img_svg, img_png) }
